1781b2752a44e941502dfbbe9a99fce32f149bdb
Nikita Malakhov <i_am_malahov@mail.ru>
2021-11-11 11:53:03 +0300

    Add project files.

---
+++ Helpers.h
@@ -0,0 +1,7 @@
+
+int math(int a, int b)
+{
+	int sum = a + b;
+	int mult = sum * sum;
+	return mult;
+}
---
+++ Homework13.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+#include "Helpers.h"
+
+int main()
+{
+    int result = math(3, 4);
+    std::cout << "Result = " << result << std::endl;
+}
